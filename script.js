02f95f6e2c1d23b0224cb3d7e636fdcc152aea6b
scaulfield01 <s.caulfield88@gmail.com>
2016-02-24 12:12:03 -0800

    add basic array capture for record feature

--- script.js
+++ script.js
@@ -1,8 +1,28 @@
 $( document ).ready(function() {
-  $('.note').click(function() {
-    var noteToPlay = $(this).attr("id") + ".wav"
+
+  var recordArray = [];
+
+  $(".note").click(function() {
+    var noteToPlayClick = $(this).attr("id") + ".wav"
+    recordArray.push(noteToPlayClick)
+    notePlayer(noteToPlayClick)
+    console.log(recordArray)
+  });
+
+  $(".record-play").click(function(){
+    recordPlay(recordArray)
+  })
+
+
+  function recordPlay(recordArray){
+    for (var i = 0; i < recordArray.length; i++) {
+      notePlayer(recordArray[i])
+    };
+  }
+
+  function notePlayer(noteToPlay){
     var audioElement = document.createElement('audio');
     audioElement.setAttribute('src', 'sounds/'+ noteToPlay);
     audioElement.play();
-  });
+  }
 })
