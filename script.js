8010bd11e1bdbb0b1f1525d02402eea0d9ac691e
scaulfield01 <s.caulfield88@gmail.com>
2016-02-24 11:40:24 -0800

    add octave wav files from personal DAW

--- script.js
+++ script.js
@@ -3,29 +3,12 @@ $( document ).ready(function() {
   //   alert($(this).attr("id"))
   // })
 
-// solution from online
-        var audioElement = document.createElement('audio');
+        // var audioElement = document.createElement('audio');
+        // audioElement.setAttribute('src', 'http://www.uscis.gov/files/nativedocuments/Track%2093.mp3');
 
-        //Change the resource to a local wav file repo
-        audioElement.setAttribute('src', 'http://www.uscis.gov/files/nativedocuments/Track%2093.mp3');
 
-        //AUTO PLAY NOT NEEDED
-        // audioElement.setAttribute('autoplay', 'autoplay');
-        //audioElement.load()
-        //NO BACK END SO NOT NEEDED TO GET
-        // $.get();
-        // LOAD not not necssary
-        // audioElement.addEventListener("load", function() {
+        // $('.note').click(function() {
         //     audioElement.play();
-        // }, true);
-
-// now notes will play the HTML element
-        $('.note').click(function() {
-            audioElement.play();
-        });
-
-        // $('.pause').click(function() {
-        //     audioElement.pause();
         // });
 
 })
