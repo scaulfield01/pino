1f3977f083aa12bdc58c228ff1db58e1e83239ed
scaulfield01 <s.caulfield88@gmail.com>
2016-02-24 11:39:03 -0800

    add solution to play external sound

--- script.js
+++ script.js
@@ -1,5 +1,31 @@
 $( document ).ready(function() {
-  $(".note").click(function(){
-    alert($(this).attr("id"))
-  })
+  // $(".note").click(function(){
+  //   alert($(this).attr("id"))
+  // })
+
+// solution from online
+        var audioElement = document.createElement('audio');
+
+        //Change the resource to a local wav file repo
+        audioElement.setAttribute('src', 'http://www.uscis.gov/files/nativedocuments/Track%2093.mp3');
+
+        //AUTO PLAY NOT NEEDED
+        // audioElement.setAttribute('autoplay', 'autoplay');
+        //audioElement.load()
+        //NO BACK END SO NOT NEEDED TO GET
+        // $.get();
+        // LOAD not not necssary
+        // audioElement.addEventListener("load", function() {
+        //     audioElement.play();
+        // }, true);
+
+// now notes will play the HTML element
+        $('.note').click(function() {
+            audioElement.play();
+        });
+
+        // $('.pause').click(function() {
+        //     audioElement.pause();
+        // });
+
 })
